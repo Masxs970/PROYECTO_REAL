e37650966590a20352dff4812110f35fdd8681f7
NoName <NoName@DESKTOP-I3GP44S>
2018-06-08 08:53:31 +0200

    tabla compras

--- ProyectoTienda/Compras.Designer.cs
+++ ProyectoTienda/Compras.Designer.cs
@@ -29,12 +29,17 @@
         private void InitializeComponent()
         {
             this.ComprasGrid = new System.Windows.Forms.DataGridView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.SalirCompras = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
             this.ComprasButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.DescripcionBox = new System.Windows.Forms.TextBox();
+            this.PrecioBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ProductoBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FechaBox = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.ComprasGrid)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,35 +48,12 @@
             this.ComprasGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.ComprasGrid.Location = new System.Drawing.Point(12, 12);
             this.ComprasGrid.Name = "ComprasGrid";
-            this.ComprasGrid.Size = new System.Drawing.Size(381, 246);
+            this.ComprasGrid.Size = new System.Drawing.Size(408, 234);
             this.ComprasGrid.TabIndex = 0;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(449, 77);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(263, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(408, 80);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(31, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Total";
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(449, 22);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(263, 20);
-            this.dateTimePicker1.TabIndex = 3;
             // 
             // SalirCompras
             // 
-            this.SalirCompras.Location = new System.Drawing.Point(637, 259);
+            this.SalirCompras.Location = new System.Drawing.Point(345, 262);
             this.SalirCompras.Name = "SalirCompras";
             this.SalirCompras.Size = new System.Drawing.Size(75, 23);
             this.SalirCompras.TabIndex = 5;
@@ -79,39 +61,110 @@
             this.SalirCompras.UseVisualStyleBackColor = true;
             this.SalirCompras.Click += new System.EventHandler(this.SalirCompras_Click);
             // 
+            // ComprasButton
+            // 
+            this.ComprasButton.Location = new System.Drawing.Point(12, 262);
+            this.ComprasButton.Name = "ComprasButton";
+            this.ComprasButton.Size = new System.Drawing.Size(97, 23);
+            this.ComprasButton.TabIndex = 7;
+            this.ComprasButton.Text = "Mostrar Compras";
+            this.ComprasButton.UseVisualStyleBackColor = true;
+            this.ComprasButton.Click += new System.EventHandler(this.ComprasButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(426, 144);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 29;
+            this.label5.Text = "Descripcion";
+            // 
+            // DescripcionBox
+            // 
+            this.DescripcionBox.Location = new System.Drawing.Point(499, 144);
+            this.DescripcionBox.Name = "DescripcionBox";
+            this.DescripcionBox.Size = new System.Drawing.Size(238, 20);
+            this.DescripcionBox.TabIndex = 27;
+            // 
+            // PrecioBox
+            // 
+            this.PrecioBox.Location = new System.Drawing.Point(499, 98);
+            this.PrecioBox.Name = "PrecioBox";
+            this.PrecioBox.Size = new System.Drawing.Size(238, 20);
+            this.PrecioBox.TabIndex = 24;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(426, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Precio";
+            // 
+            // ProductoBox
+            // 
+            this.ProductoBox.Location = new System.Drawing.Point(499, 50);
+            this.ProductoBox.Name = "ProductoBox";
+            this.ProductoBox.Size = new System.Drawing.Size(238, 20);
+            this.ProductoBox.TabIndex = 22;
+            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(408, 28);
+            this.label2.Location = new System.Drawing.Point(426, 57);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Filtrar";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Producto";
             // 
-            // ComprasButton
+            // label1
             // 
-            this.ComprasButton.Location = new System.Drawing.Point(547, 259);
-            this.ComprasButton.Name = "ComprasButton";
-            this.ComprasButton.Size = new System.Drawing.Size(75, 23);
-            this.ComprasButton.TabIndex = 7;
-            this.ComprasButton.Text = "Compras";
-            this.ComprasButton.UseVisualStyleBackColor = true;
-            this.ComprasButton.Click += new System.EventHandler(this.ComprasButton_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(426, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Fecha";
+            // 
+            // FechaBox
+            // 
+            this.FechaBox.Location = new System.Drawing.Point(499, 15);
+            this.FechaBox.Name = "FechaBox";
+            this.FechaBox.Size = new System.Drawing.Size(238, 20);
+            this.FechaBox.TabIndex = 31;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(171, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 32;
+            this.button1.Text = "Nueva Compra";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // ViewCompras
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(722, 294);
-            this.Controls.Add(this.ComprasButton);
+            this.ClientSize = new System.Drawing.Size(752, 294);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.FechaBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.DescripcionBox);
+            this.Controls.Add(this.PrecioBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ProductoBox);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.SalirCompras);
-            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ComprasButton);
+            this.Controls.Add(this.SalirCompras);
             this.Controls.Add(this.ComprasGrid);
             this.Name = "ViewCompras";
             this.Text = "Compras";
+            this.Load += new System.EventHandler(this.ViewCompras_Load);
             ((System.ComponentModel.ISupportInitialize)(this.ComprasGrid)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -121,11 +174,16 @@
         #endregion
 
         private System.Windows.Forms.DataGridView ComprasGrid;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Button SalirCompras;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button ComprasButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox DescripcionBox;
+        private System.Windows.Forms.TextBox PrecioBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ProductoBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker FechaBox;
+        private System.Windows.Forms.Button button1;
     }
 }
--- ProyectoTienda/Compras.cs
+++ ProyectoTienda/Compras.cs
@@ -14,6 +14,7 @@ namespace ProyectoTienda
 {
     public partial class ViewCompras : Form
     {
+        public SqlConnection cn = new SqlConnection("Data Source=DESKTOP-I3GP44S;Initial Catalog=sistema_tienda;Integrated Security=True");
         public ViewCompras()
         {
             InitializeComponent();
@@ -24,15 +25,28 @@ namespace ProyectoTienda
             this.Close();
         }
 
+        //void Buscar()
+        //{
+        //    SqlDataAdapter da = new SqlDataAdapter("sp_facturacion", cn);
+        //    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+        //    da.SelectCommand.Parameters.Add("@fechastart", SqlDbType.DateTime).Value = desde.Text;
+        //    da.SelectCommand.Parameters.Add("@fechaend", SqlDbType.DateTime).Value = hasta.Text;
+        //    DataTable dt = new DataTable();
+        //    da.Fill(dt);
+        //    this.ComprasGrid.DataSource = dt;
+
+        //}
+
         private void ComprasButton_Click(object sender, EventArgs e)
         {
             //Leer archivo configuracion
             ConnectionStringSettings setting = ConfigurationManager.ConnectionStrings["ProyectoTienda.Properties.Settings.sistema_tiendaConnectionString"];
             //conectar con la base de datos
             SqlConnection connection = new SqlConnection(setting.ConnectionString);
-            //Seleccionar los campos de operación
+            //Escribimos la sentencia que necesitamos
             string sentencia = "exec sp_facturacion";
             SqlCommand x = new SqlCommand(sentencia, connection);
+            
             //Mostrarlos en la grid
             try
             {
@@ -53,6 +67,96 @@ namespace ProyectoTienda
             }
 
         }
+
+
+
+        private void ViewCompras_Load(object sender, EventArgs e)
+        {
+            //Leer archivo configuracion
+            ConnectionStringSettings setting = ConfigurationManager.ConnectionStrings["ProyectoTienda.Properties.Settings.sistema_tiendaConnectionString"];
+            //conectar con la base de datos
+            SqlConnection connection = new SqlConnection(setting.ConnectionString);
+            string sentencia2 = "SELECT * FROM compras";
+            SqlCommand x = new SqlCommand(sentencia2, connection);
+            try
+            {
+                //Abrir la conexión
+                connection.Open();
+                //Ejecutar comando
+                SqlDataReader rdr = x.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(rdr);
+                this.ComprasGrid.DataSource = dt;
+                //Cierro el Datareader, cerrara tb la conexión
+                rdr.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Se ha producido un error" + ex.Message);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ////Leer Archivo Configuracion
+            //ConnectionStringSettings setting = ConfigurationManager.ConnectionStrings["ProyectoTienda.Properties.Settings.sistema_tiendaConnectionString"];
+
+            ////Conectarme con la bbdd
+            //SqlConnection conn = new SqlConnection(setting.ConnectionString); //Elemento 
+
+            ////Seleccionar la tabla 
+            //string query1 = "INSERT INTO sistema_tienda.dbo.sp_facturacion" + "([fecha], [nombre], [precio], [descripcion]) VALUES (" + FechaBox.Value + ", '" + ProductoBox.Text + "', " + PrecioBox.Text + ", '" + DescripcionBox.Text + "' )";
+
+
+            //SqlCommand InsertData = new SqlCommand(query1,  conn);  //Comando SQL 
+
+
+            //try
+            //{
+            //    //Abrir la conexion
+            //    conn.Open();
+
+            //    InsertData.ExecuteNonQuery();
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show("No se ha podido añadir la nueva compra" + ex.Message);
+            //}
+
+
+            try
+            {
+
+                SqlConnection Conexion = new SqlConnection("Data Source=DESKTOP-I3GP44S;Initial Catalog=sistema_tienda;Integrated Security=True");
+
+                SqlCommand cmd = new SqlCommand("sp_InsertaCompra", Conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add("@fecha", SqlDbType.DateTime).Value = FechaBox.Text;
+                cmd.Parameters.Add("@producto", SqlDbType.VarChar).Value = ProductoBox.Text;
+                cmd.Parameters.Add("@precio", SqlDbType.Decimal).Value = PrecioBox.Text;
+                cmd.Parameters.Add("@descripcion", SqlDbType.VarChar).Value = DescripcionBox.Text;
+
+
+                Conexion.Open();
+
+                cmd.ExecuteNonQuery();
+
+                Conexion.Close();
+                MessageBox.Show("Datos insertados correctamente");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(" Datos No Insertados" + ex.Message);
+            }
+        }
+
     }
+
 }
+    
+
 
